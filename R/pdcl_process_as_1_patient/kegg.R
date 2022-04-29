e3237d3a5af7b6e0e4ef75e4e44d8c5debd1ccf5
Kévin SPINICCI <kevin.spinicci@outlook.com>
2022-04-29 18:27:40 +0200

    Kegg script to download entries and get categories

---
+++ R/pdcl_process_as_1_patient/kegg.R
@@ -0,0 +1,39 @@
+library(KEGGREST)
+library(stringr)
+
+KEGG_TOP_CATEGORIES <- c(
+  "Cellular Processes",
+  "Drug Development",
+  "Environmental Information Processing",
+  "Genetic Information Processing",
+  "Human Diseases",
+  "Metabolism",
+  "Organismal Systems",
+  "No Category"
+)
+
+get.kegg.entries <- function(kegg.ids){
+  entries <- list()
+  ids <- kegg.ids
+  while ( !isEmpty(ids) ) {
+    n <- min(10, length(ids))
+    keggres <- keggGet(ids[0:n])
+    entries <- c(entries, keggres)
+    ids <- ids[0:-n]
+  }
+  names(entries) <- kegg.ids
+  entries
+}
+
+get.kegg.categ <- function(kegg.entries){
+  categories <- lapply(kegg.entries, function(x){
+    str <- x$CLASS
+    if(is.null(str)){
+      str <- "No Category"
+    }
+    str
+  })
+  categories <- str_split(categories, ";\\s*")
+  names(categories) <- names(kegg.entries)
+  categories
+}
