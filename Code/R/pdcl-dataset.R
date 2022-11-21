8d91d9eca39975419b6d298eaf63e70b953732a1
Kévin SPINICCI <kevin.spinicci@outlook.com>
2022-11-21 12:42:42 +0100

    Add script for performing PenDA analysis on PDCL

---
+++ Code/R/pdcl-dataset.R
@@ -0,0 +1,59 @@
+# This file contain all the variables useful for working with the PDCL dataset
+
+PDCL_DATASET_PARAM <- list(
+  ##### Control Parameters
+  "control_count_path" = "~/PhD/Project/PDCL/Data/PDCL/astrocyte_GSE109001_counts_unique_genes_samples_filtered.txt",
+  "control_sample_names" = c(
+    "AF22_NES_Astro_Br1_d0_01_S1",
+    "AF22_NES_Astro_Br1_d29_37_S46",
+    "AF22_NES_Astro_Br2_d0_02_S12",
+    "AF22_NES_Astro_Br2_d29_38_S56",
+    "AF22_NES_Astro_Br3_d0_03_S22",
+    "AF22_NES_Astro_Br3_d29_39_S66",
+    "C1_ltNES_Br1_d29_76_S41",
+    "C1_ltNES_Br2_d29_77_S51",
+    "C1_ltNES_Br3_d29_78_S61",
+    "C1_NES_Astro_Br1_d0_04_S32",
+    "C1_NES_Astro_Br2_d0_05_S42",
+    "C1_NES_Astro_Br3_d0_06_S52",
+    "C9_ltNES_Br1_d29_79_S71",
+    "C9_ltNES_Br2_d29_80_S81",
+    "C9_ltNES_Br3_d29_81_S11",
+    "C9_NES_Astro_Br1_d0_07_S62",
+    "C9_NES_Astro_Br2_d0_08_S72",
+    "C9_NES_Astro_Br3_d0_09_S2",
+    "CCF.STTG1_p24_Br1_S16",
+    "CCF.STTG1_p24_Br2_S17",
+    "CCF.STTG1_p24_Br3_S18",
+    "CDIAstrocytes_p2_Br1_S19",
+    "CDIAstrocytes_p2_Br2_S20",
+    "CDIAstrocytes_p2_Br3_S21",
+    "phaAstrocyte_p2_Br1_S1",
+    "phaAstrocyte_p2_Br2_S2",
+    "phaAstrocyte_p2_Br3_S3"
+  ),
+  ##### PDCL Parameters
+  "pdcl_count_path" = "~/PhD/Project/PDCL/Data/PDCL/lignees_count_genes_PDCL.txt",
+  "pdcl_sample_names" = c(
+    "4339-p21",
+    "4371-p37",
+    "5706-p14",
+    "6190-p43",
+    "6240-p12",
+    "7015-p17",
+    "7060-p18", 
+    "7142-p14",
+    "N13-1300",
+    "N13-1520-p9",
+    "N14-0072",
+    "N14-0870",
+    "N14-1208",
+    "N14-1525",
+    "N15_0460",
+    "N15_0516",
+    "N15-0385",
+    "N15-0661",
+    "N16_0535",
+    "N16-0240"
+  )
+)
