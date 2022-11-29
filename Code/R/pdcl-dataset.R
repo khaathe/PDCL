97156935676f816473d0c14c15532041b7d8fab8
Kévin SPINICCI <kevin.spinicci@outlook.com>
2022-11-29 11:49:58 +0100

    Update control samples and count files path

--- Code/R/pdcl-dataset.R
+++ Code/R/pdcl-dataset.R
@@ -2,7 +2,7 @@
 
 PDCL_DATASET_PARAM <- list(
   ##### Control Parameters
-  "control_count_path" = "~/PhD/Project/PDCL/Data/PDCL/astrocyte_GSE109001_counts_unique_genes_samples_filtered.txt",
+  "control_count_path" = "~/PhD/Project/PDCL/Data/pdcl-dataset/control/controls_pdcl_datasets.csv",
   "control_sample_names" = c(
     "AF22_NES_Astro_Br1_d0_01_S1",
     "AF22_NES_Astro_Br1_d29_37_S46",
@@ -10,30 +10,15 @@ PDCL_DATASET_PARAM <- list(
     "AF22_NES_Astro_Br2_d29_38_S56",
     "AF22_NES_Astro_Br3_d0_03_S22",
     "AF22_NES_Astro_Br3_d29_39_S66",
-    "C1_ltNES_Br1_d29_76_S41",
-    "C1_ltNES_Br2_d29_77_S51",
-    "C1_ltNES_Br3_d29_78_S61",
     "C1_NES_Astro_Br1_d0_04_S32",
     "C1_NES_Astro_Br2_d0_05_S42",
     "C1_NES_Astro_Br3_d0_06_S52",
-    "C9_ltNES_Br1_d29_79_S71",
-    "C9_ltNES_Br2_d29_80_S81",
-    "C9_ltNES_Br3_d29_81_S11",
     "C9_NES_Astro_Br1_d0_07_S62",
     "C9_NES_Astro_Br2_d0_08_S72",
-    "C9_NES_Astro_Br3_d0_09_S2",
-    "CCF.STTG1_p24_Br1_S16",
-    "CCF.STTG1_p24_Br2_S17",
-    "CCF.STTG1_p24_Br3_S18",
-    "CDIAstrocytes_p2_Br1_S19",
-    "CDIAstrocytes_p2_Br2_S20",
-    "CDIAstrocytes_p2_Br3_S21",
-    "phaAstrocyte_p2_Br1_S1",
-    "phaAstrocyte_p2_Br2_S2",
-    "phaAstrocyte_p2_Br3_S3"
+    "C9_NES_Astro_Br3_d0_09_S2"
   ),
   ##### PDCL Parameters
-  "pdcl_count_path" = "~/PhD/Project/PDCL/Data/PDCL/lignees_count_genes_PDCL.txt",
+  "pdcl_count_path" = "~/PhD/Project/PDCL/Data/pdcl-dataset/PDCL/lignees_count_genes_PDCL.csv",
   "pdcl_sample_names" = c(
     "4339-p21",
     "4371-p37",
